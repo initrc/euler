50900eb6645c6f6d504008a464a919a2e41c1022
David Shi <DavidSnooker@gmail.com>
2013-04-16 23:57:22 -0700

    problem 4 python

--- 1-10.py
+++ 1-10.py
@@ -33,7 +33,44 @@ def p3():
       factor += 1
   print max_factor if x == 1 else x
 
+# Largest palindrome product
+def p4():
+  a, max, x1, x2 = 999 , -1, -1, -1
+  # if 6 digit, must be a multiple of 11
+  # 111111 = 777 * 143
+  for i in range(990, 109, -11):
+  # or brute force
+  # for i in range(a, 0, -1):
+    for j in range(a, 0, -1):
+      p = i * j
+      if p < max:
+        if j == a:
+          print "%d = %d * %d" % (max, x1, x2)
+          return
+        else:
+          break
+      if p4_is_palindrome(p):
+        max, x1, x2 = p, i, j;
+
+def p4_is_palindrome(x):
+  if x < 0:
+    return False
+  if x < 10:
+    return True
+  digit = int(math.log(x, 10)) + 1
+  base = int(math.pow(10, digit - 1))
+  reverse_x = 0;
+  multi = 1;
+  num = x
+  for i in range(digit):
+    reverse_x += num / base * multi
+    num %= base
+    base /= 10
+    multi *= 10
+  return x == reverse_x
+
 if __name__ == '__main__':
   p1()
   p2()
   p3()
+  p4()
