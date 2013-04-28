3b5e4c1245383601fb20ba962d0cfc1127068af5
David Shi <DavidSnooker@gmail.com>
2013-04-28 00:35:17 -0700

    problem 18 python

--- p11_20.py
+++ p11_20.py
@@ -299,6 +299,47 @@ class P11_20(Problem):
         else:
             return 0  # unsupported
 
+    def p18(self):
+        """Maximum path sum I"""
+        """Maximum path sum II at p67"""
+        tree = self._p18_build_list()
+        for i, row in enumerate(tree[:-1]):
+            for j, node in enumerate(row):
+                node.visit()
+                if tree[i + 1][j].value_above < node.value:
+                    tree[i + 1][j].value_above = node.value
+                tree[i + 1][j + 1].value_above = node.value
+        print max(x.value_above + x.value for x in tree[-1])
+
+    def _p18_build_list(self):
+        input = [
+            "75",
+            "95 64",
+            "17 47 82",
+            "18 35 87 10",
+            "20 04 82 47 65",
+            "19 01 23 75 03 34",
+            "88 02 77 73 07 63 67",
+            "99 65 04 28 06 16 70 92",
+            "41 41 26 56 83 40 80 70 33",
+            "41 48 72 33 47 32 37 16 94 29",
+            "53 71 44 65 25 43 91 52 97 51 14",
+            "70 11 33 28 77 73 17 78 39 68 17 57",
+            "91 71 52 38 17 14 91 43 58 50 27 29 48",
+            "63 66 04 68 89 53 67 30 73 16 69 87 40 31",
+            "04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"]
+        return [map(lambda x: TreeNode(int(x)), x.split()) for x in input]
+
+
+class TreeNode:
+
+    def __init__(self, value):
+        self.value = value
+        self.value_above = 0
+
+    def visit(self):
+        self.value += self.value_above
+
 if __name__ == '__main__':
     p = P11_20()
     p.solve_largest()
