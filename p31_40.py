11f6c9e7177612ddc3dc886a8ce9b0b0b7d67e25
David Shi <DavidSnooker@gmail.com>
2013-05-06 02:29:38 -0700

    problem 35 python

--- p31_40.py
+++ p31_40.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import math
+from sets import Set
 
+from p1_10 import P1_10
 from problem import Problem
 
 
@@ -29,6 +31,29 @@ class P31_40(Problem):
                     break
         print dsum
 
+    def p35(self):
+        """Circular primes"""
+        LIMIT = 1000000
+        primes = P1_10()._prime_set(LIMIT)
+        circular_primes = Set()
+        digitnum = lambda x: int(math.log10(x) + 1)
+        rotate = lambda x, d: x % 10 * 10**d + x / 10
+        for x in primes:
+            if x in circular_primes:
+                continue
+            circular = True
+            nrotate = digitnum(x) - 1
+            for d in xrange(nrotate):
+                x = rotate(x, nrotate)
+                if x not in primes:
+                    circular = False
+                    break
+            if circular:
+                for d in xrange(nrotate + 1):
+                    circular_primes.add(x)
+                    x = rotate(x, nrotate)
+        print len(circular_primes)
+
 if __name__ == '__main__':
     p = P31_40()
-    p.solve_all()
+    p.solve_largest()
