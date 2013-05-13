cb80fbdf6224ca9b156e3689b6d03c4ee61b0670
David Shi <DavidSnooker@gmail.com>
2013-05-12 17:41:14 -0700

    problem 42 python

--- p41_50.py
+++ p41_50.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import itertools
+from sets import Set
 
 from p1_10 import P1_10
 from problem import Problem
@@ -21,6 +22,19 @@ class P41_50(Problem):
                     print i
                     return
             del pandigital[:]
+
+    def p42(self):
+        """Coded triangle numbers"""
+        count = 0
+        tri_nums = Set()
+        for i in xrange(1, 100):
+            tri_nums.add(i * (i+1) / 2)
+        with open('data/p42_words.txt', 'r') as f:
+            words = [s.strip('\".\"') for s in f.read().split(',')]
+            for word in words:
+                if sum([ord(c) - 64 for c in word]) in tri_nums:
+                    count += 1
+        print count
 
 if __name__ == '__main__':
     p = P41_50()
